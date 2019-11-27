a02a396991f9f3e73a232d3b55b23fa0b0942fa5
mahendrakundare <mr.kundare@gmail.com>
2019-11-27 09:38:40 +0530

    Refactor : remove commented code

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -4,13 +4,13 @@ public class MoodAnalyzer {
 
     private String message;
 
-//    public MoodAnalyzer() {
-//
-//    }
-//
-//    public MoodAnalyzer(String message) {
-//        this.message = message;
-//    }
+    public MoodAnalyzer() {
+
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
 
     public String analyzeMood() throws MoodAnalysisException {
         try {
